5c8a33bb4f894e238c7874912b29547eb3104c2e
lofidewanto <lofidewanto@gmail.com>
2018-06-19 10:44:59 +0200

    update

--- restygwt-enum-date-client-gwtui/src/main/java/com/example/client/BasicGwtEntryPoint.java
+++ restygwt-enum-date-client-gwtui/src/main/java/com/example/client/BasicGwtEntryPoint.java
@@ -16,15 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.example.demo;
+package com.example.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
-@SpringBootApplication
-public class RestygwtDateEnumServerApplication {
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestygwtDateEnumServerApplication.class, args);
+public class BasicGwtEntryPoint implements EntryPoint {
+
+	private static Logger logger = Logger.getLogger(BasicGwtEntryPoint.class.getName());
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			Window.alert("Hello World!");
+			logger.info("Hello World!");
+		});
+
+		RootPanel.get("helloButton").add(button);
 	}
 }
