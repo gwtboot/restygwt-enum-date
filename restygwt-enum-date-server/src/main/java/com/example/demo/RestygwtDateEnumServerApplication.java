5dbcf3572c024f1882f247c35fa977e243114851
lofidewanto <lofidewanto@gmail.com>
2018-06-19 09:56:14 +0200

    init

---
+++ restygwt-enum-date-server/src/main/java/com/example/demo/RestygwtDateEnumServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestygwtDateEnumServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestygwtDateEnumServerApplication.class, args);
+	}
+}
