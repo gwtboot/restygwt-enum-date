eddfdc3eda5676bc7a9a849d130f5ccb7439bddb
lofidewanto <lofidewanto@gmail.com>
2018-06-19 14:40:48 +0200

    update

--- restygwt-enum-date-api/src/main/java/com/example/api/PersonDto.java
+++ restygwt-enum-date-api/src/main/java/com/example/api/PersonDto.java
@@ -21,6 +21,7 @@ package com.example.api;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class PersonDto {
 
@@ -31,7 +32,7 @@ public class PersonDto {
 
 	// With JsonIgnore it works, without you get RestyGWT error: Not a valid JSON
 	// format!
-	// @JsonIgnore
+	@JsonIgnore
 	private PersonType personType;
 
 	public String getName() {
